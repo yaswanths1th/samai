9d4404f3ebf3a99ee896ead3ec24b64b23fd5988
yaswanths1th <yaswanths1th001@gmail.com>
2025-12-22 12:17:18 +0530

    new1

--- src/components/CTASection.jsx
+++
@@ -1,8 +0,0 @@
-export default function CTASection() {
-  return (
-    <section className="cta">
-      <h2>See Samai in action on your processes</h2>
-      <button className="btn-primary">Schedule a Demo</button>
-    </section>
-  );
-}
